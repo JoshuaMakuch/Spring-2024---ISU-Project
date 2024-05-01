2785bfe7512c10c6d3960b432d093e0191a109ca
JoshuaMakuch <122292209+JoshuaMakuch@users.noreply.github.com>
2024-05-01 13:13:12 -0600

    Code - ATV_TurretControl.X

---
+++ ATV_TurretControl.X/build/default/production/TurretControl.o.d
@@ -0,0 +1,4 @@
+build/default/production/TurretControl.o:  \
+	C:/Users/Baden\ Brenner/OneDrive/Documents/5th\ semester/ATV_TurretControl.X/TurretControl.asm  \
+	C:/Users/Baden\ Brenner/OneDrive/Documents/5th\ semester/ATV_TurretControl.X/p16f1788.inc  \
+
